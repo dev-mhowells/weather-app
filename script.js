1c47045bfe960b7f2d24135263e0a32ff7f41899
Mike <dev.mhowells@gmail.com>
2022-03-26 16:02:12 +0000

    fully functioning and finished design

--- script.js
+++ script.js
@@ -7,6 +7,14 @@ const dispDescription = document.getElementById("display-description");
 const dispTemp = document.getElementById("display-temp");
 
 const bottom = document.getElementById("bottom");
+const extraData = document.getElementById("extra-data");
+
+const feelsVal = document.createElement("p");
+const humidityVal = document.createElement("p");
+const windVal = document.createElement("p");
+
+// const localTime = document.getElementById("local-time");
+// const left = document.getElementById("left");
 
 async function getCoords(country) {
   const response = await fetch(
@@ -64,47 +72,6 @@ function createDailySummaries(allData) {
   return days;
 }
 
-function createDomEls(arr) {
-  const dailyDisplay = document.createElement("div");
-  dailyDisplay.classList.add("daily-display");
-  bottom.appendChild(dailyDisplay);
-  // put loop in here
-  const tomDescription = arr[1].description;
-  console.log(tomDescription);
-  const dailyDescription = document.createElement("h4");
-  dailyDescription.classList.add("description");
-  dailyDescription.textContent = tomDescription;
-  dailyDisplay.appendChild(dailyDescription);
-
-  //   data.appendChild(testEl);
-  //   testEl.textContent = tomDescription;
-}
-
-function createDomElsHtml(arr) {
-  const html = `<div class="daily-display" id="daily-display">
-  <h4 class="day" id="day">Oneday</h4>
-  <h4 class="description" id="description">${arr[1].description}</h4>
-  <div class="min-max" id="min-max">
-    <h5 class="max" id="max">Max -</h5>
-    <p class="max-val" id="max-val">${Math.round(arr[1].tempMax)}°</p>
-    <h5 class="min" id="min">- Min</h5>
-    <p class="min-val" id="min-val">${Math.round(arr[1].tempMin)}°</p>
-  </div>
-  <h4 class="wind" id="wind">Wind: ${arr[1].windSpd}</h4>
-</div>`;
-  bottom.insertAdjacentHTML("beforeend", html);
-}
-
-function centerDisplay(obj) {
-  dispLocation.textContent = locationInput.value;
-  dispDescription.textContent = obj.description;
-  dispTemp.textContent = `${Math.round(obj.temp)}°C`;
-}
-
-// function bottomDisplay(obj) {
-//     const html =
-// }
-
 const now = new Date();
 const dayOfWeekArray = [
   "Sunday",
@@ -115,22 +82,75 @@ const dayOfWeekArray = [
   "Friday",
   "Saturday",
 ];
-const dayOfWeek = now.getDay();
-console.log(dayOfWeekArray[dayOfWeek]);
+
+function createDomElsHtml(arr) {
+  // function takes array and populates bottom of page with daily summaries
+  const dayOfWeek = now.getDay();
+  let n = dayOfWeek + 1; // because daily display starts from tomorrow
+  for (let i = 1; i <= 7; i++) {
+    // adjusts day of the week
+    if (n < 7) {
+      n++;
+    } else if (n >= 7) {
+      n = 0;
+    }
+    // dynamic daily summary elements
+    const html = `<div class="daily-display" id="daily-display">
+  <h4 class="day" id="day">${dayOfWeekArray[n]}</h4>
+  <h4 class="description" id="description">${arr[i].description}</h4>
+  <div class="min-max" id="min-max">
+    <h5 class="max" id="max">Max -</h5>
+    <p class="max-val" id="max-val">${Math.round(arr[i].tempMax)}°</p>
+    <h5 class="min" id="min">- Min</h5>
+    <p class="min-val" id="min-val">${Math.round(arr[i].tempMin)}°</p>
+  </div>
+  <h4 class="wind" id="wind">wind ${Math.round(arr[i].windSpd * 2.237)} mph</h4>
+</div>`;
+    bottom.insertAdjacentHTML("beforeend", html);
+  }
+}
+
+function centerDisplay(obj) {
+  dispLocation.textContent = locationInput.value;
+  dispDescription.textContent = obj.description;
+  dispTemp.textContent = `${Math.round(obj.temp)}°C`;
+}
+
+function leftDisplay(obj) {
+  feelsVal.classList.add("left-feels-value");
+  feelsVal.textContent = `${Math.round(obj.feelsLike)}°C`;
+  extraData.appendChild(feelsVal);
+
+  humidityVal.classList.add("left-humidity-value");
+  humidityVal.textContent = `${obj.humidity}%`;
+  extraData.appendChild(humidityVal);
+
+  windVal.classList.add("left-wind-value");
+  windVal.textContent = `${Math.round(obj.windSpd * 2.237)} mph`;
+  extraData.appendChild(windVal);
+}
+
+// function setTime(obj) {}
 
 async function fulfillSearch(loc) {
   const coords = await getCoords(loc);
   const allData = await getWeather(coords[0], coords[1]);
+  bottom.innerHTML = "";
   console.log(allData);
   console.log(filterData(allData)); // for current weather, returns object
   currentWeatherObj = filterData(allData);
   centerDisplay(currentWeatherObj);
+  leftDisplay(currentWeatherObj);
   const allSummaries = createDailySummaries(allData); // for daily weather for week, returns array of objects
   console.log(allSummaries);
-  createDomEls(allSummaries); // takes array of objects (daily summaries) and puts on page
-  createDomElsHtml(allSummaries);
+  createDomElsHtml(allSummaries); // takes array of objects (daily summaries) and puts on page
+  locationInput.value = "";
 }
 
+locationInput.value = "london";
+fulfillSearch("london");
+
 searchBtn.addEventListener("click", function () {
+  console.log(locationInput.value.toLowerCase());
   fulfillSearch(locationInput.value);
 });
